183192059a6de9ac314d4ea75b266c07ef1b8471
Chalon Hutson <chalonhutson@gmail.com>
2023-08-26 22:05:38 -0600

    Set up for JWT auth.

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -2,17 +2,20 @@ import os
 from flask import Flask
 from flask_migrate import Migrate
 
-from .extensions import api, db
+from .extensions import api, db, jwt
 from .resources import ns
 
 def create_app():
     app = Flask(__name__)
 
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["POSTGRES_URI"]
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["JWT_SECRET_KEY"] = os.environ["JWT_SECRET_KEY"]
 
     api.init_app(app)
     db.init_app(app)
     Migrate(app, db)
+    jwt.init_app(app)
 
     api.add_namespace(ns)
 
--- server/app/resources.py
+++ server/app/resources.py
@@ -1,18 +1,35 @@
 from flask_restx import Resource, Namespace
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
 from .extensions import db
 from .models import Volunteer, Event
 
-ns = Namespace("api")
+authorizations = {
+    "jsonWebToken": {
+        "type": "apiKey",
+        "in": "header",
+        "name": "Authorization"
+    }
+}
+
+ns = Namespace("api", authorizations=authorizations)
 
 @ns.route("/volunteers")
 class Volunteers(Resource):
+    method_decorators = [jwt_required()]
+
+    @ns.doc(security="jsonWebToken")
     def get(self):
+        print(get_jwt_identity())
         volunteers = Volunteer.query.all()
         return [volunteer.get_dict() for volunteer in volunteers]
 
 @ns.route("/events")
 class Events(Resource):
+    method_decorators = [jwt_required()]
+
+    @ns.doc(security="jsonWebToken")
     def get(self):
+        print(get_jwt_identity())
         events = Event.query.all()
         return [event.get_dict() for event in events]
