c742ce37b6bd0fec29edbd84bb8a3854c1c4390c
Chalon Hutson <chalonhutson@gmail.com>
2023-08-31 17:07:06 -0600

    Added phones to add volunteers. Not finished.

--- client/src/components/pages/AddVolunteer.jsx
+++ client/src/components/pages/AddVolunteer.jsx
@@ -2,26 +2,34 @@ import { useState, useRef } from "react"
 import { Link } from "react-router-dom"
 
 export default function AddVolunteer() {
-    // const [phoneCount, setPhoneCount] = useState(1)
-    // console.log(phoneCount)
+    const [phoneInputs, setPhoneInputs] = useState([])
 
-    // function renderPhoneInputs() {
-    //     const inputs = []
-
-    //     for (let i = 0; i < phoneCount; i++) {
-    //         inputs.push(
-    //             <div key={i}>
-    //                 <label htmlFor="phone">Phone</label>
-    //                 <input className="form-control" type="text" name="phone" id="phone" />
-    //                 <button className="btn btn-danger">-</button>
-    //             </div>
-    //         )
-    //     }
-    //     return inputs
-    // }
+    function renderPhoneInputs() {
+        return phoneInputs.map((input, i) => {
+            return (
+                <div key={i}>
+                    <label htmlFor="phone">Phone</label>
+                    <input className="form-control" type="text" name="phone" id="phone" defaultValue={input.value} />
+                    <button onClick={() => removePhoneInput(i)} className="btn btn-danger">-</button>
+                </div>
+            )
+        }
+        )
+    }
 
     const firstNameRef = useRef()
     const lastNameRef = useRef()
+
+    function addPhoneInput() {
+        setPhoneInputs([...phoneInputs, { value: "" }])
+        console.log(phoneInputs)
+    }
+
+    function removePhoneInput(i) {
+        const newPhoneInputs = phoneInputs.filter((_, i) => i !== index);
+        setPhoneInputs(newPhoneInputs)
+        console.log("remove phone input")
+    }
 
     function handleSubmit(e) {
         e.preventDefault()
@@ -53,14 +61,14 @@ export default function AddVolunteer() {
                 <input ref={firstNameRef} className="form-control" type="text" name="name" id="name" />
                 <label htmlFor="last-name">Last Name</label>
                 <input ref={lastNameRef} className="form-control" type="text" name="last-name" id="last-name" />
-                {/* <label htmlFor="preferred-contact">Preferred Contact</label> */}
-                {/* <select className="form-select" name="preferred-contact" id="preferred-contact"> */}
-                {/* <option value="email">Email</option> */}
-                {/* <option value="phone">Phone</option> */}
-                {/* </select> */}
-                {/* <label htmlFor="add-phone">Phone</label> */}
-                {/* <button onClick={() => setPhoneCount((count) => count + 1)} className="btn btn-primary" type="button" id="add-phone">+</button> */}
-                {/* {renderPhoneInputs()} */}
+                <label htmlFor="preferred-contact">Preferred Contact</label>
+                <select className="form-select" name="preferred-contact" id="preferred-contact">
+                    <option value="email">Email</option>
+                    <option value="phone">Phone</option>
+                </select>
+                <label htmlFor="add-phone">Phone</label>
+                <button onClick={() => addPhoneInput()} className="btn btn-primary" type="button" id="add-phone">+</button>
+                {renderPhoneInputs()}
                 <br></br>
                 <button className="btn btn-primary" type="submit">Submit</button>
             </form>
