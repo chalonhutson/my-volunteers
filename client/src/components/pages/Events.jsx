22fc353802a6150738984d42b8fdb4b31037bd1b
Chalon Hutson <chalonhutson@gmail.com>
2023-10-03 14:09:46 -0600

    Updated Events and Volunteers view away from tables towards Grid CSS.

--- client/src/components/pages/Events.jsx
+++ client/src/components/pages/Events.jsx
@@ -1,18 +1,22 @@
-import { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
+import { useState, useEffect } from "react"
+import { Link, useNavigate } from "react-router-dom"
 import { useAuthHeader } from "react-auth-kit"
+
+import "../../css/Events.css"
 
 import HeroBtn from '../buttons/HeroBtn'
 
 
 export default function Events() {
 
+    const navigate = useNavigate()
+
     const [events, setEvents] = useState([])
     const authHeader = useAuthHeader()
 
 
     useEffect(() => {
-        fetch('/api/events', {
+        fetch("/api/events", {
             method: "GET",
             headers: {
                 "Authorization": authHeader(),
@@ -28,45 +32,29 @@ export default function Events() {
             .then(data => setEvents(data))
     }, [])
 
+
     return (
-        <div>
+        <main>
             <HeroBtn route="/" text="back to dashboard" />
-            <table>
-                <tr>
-                    <th scope="col">event</th>
-                    <th scope="col">location</th>
-                    <th scope="col">time</th>
-                    <th scope="col">description</th>
-                </tr>
+            <div className="eventsGrid">
+                <div className="eventsGridHeader">
+                    <span>event</span>
+                    <span>location</span>
+                    <span>time</span>
+                    <span>description</span>
+                </div>
 
                 {events.map(event => (
-                    <Link to={`/events/${event.event_id}`}>
-                        <tr>
-                            <td className="eventData">{event.event_name}</td>
-                            <td className="eventData">{event.event_location}</td>
-                            <td className="eventData">{event.event_date}</td>
-                            <td className="eventData">{event.event_description}</td>
-                        </tr>
-                    </Link>
+                    <div onClick={() => navigate(`/events/${event.event_id}`)} className="eventsGridRow">
+                        <span className="eventData">{event.event_name}</span>
+                        <span className="eventData">{event.event_location}</span>
+                        <span className="eventData">{event.event_date}</span>
+                        <span className="eventData">{event.event_description}</span>
+                    </div>
                 ))}
+            </div>
 
-                {/* <tr>
-                    <td className="eventData">GOTV Door Knocking - LD15</td>
-                    <td className="eventData">Field Office</td>
-                    <td className="eventData">Saturday July 14, 2024</td>
-                </tr>
-                <tr>
-                    <td>Pints & Policy</td>
-                    <td>Dave & Busters</td>
-                    <td>Friday July 20, 2024</td>
-                </tr>
-                <tr>
-                    <td>GOTV Door Knocking - LD15</td>
-                    <td>Field Office</td>
-                    <td>Saturday July 14, 2024</td>
-                </tr> */}
-            </table>
             <HeroBtn route="/add-event" text="add event" />
-        </div>
+        </main>
     )
 }
--- client/src/components/pages/Volunteers.jsx
+++ client/src/components/pages/Volunteers.jsx
@@ -1,169 +1,22 @@
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
-
+import React, { useEffect, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
 import { useAuthHeader } from "react-auth-kit"
 
 import "../../css/Volunteers.css"
 
-import headshot_1 from "../../assets/headshots/headshot_1.jpg";
-import headshot_2 from "../../assets/headshots/headshot_2.jpg";
-import headshot_3 from "../../assets/headshots/headshot_3.jpg";
-import headshot_4 from "../../assets/headshots/headshot_4.jpg";
-import headshot_5 from "../../assets/headshots/headshot_5.jpg";
-import headshot_6 from "../../assets/headshots/headshot_6.jpg";
-import headshot_7 from "../../assets/headshots/headshot_7.jpg";
-import headshot_8 from "../../assets/headshots/headshot_8.jpg";
-import headshot_9 from "../../assets/headshots/headshot_9.jpg";
-import headshot_10 from "../../assets/headshots/headshot_10.jpg";
-import headshot_11 from "../../assets/headshots/headshot_11.jpg";
-import headshot_12 from "../../assets/headshots/headshot_12.jpg";
-import headshot_13 from "../../assets/headshots/headshot_13.jpg";
-import headshot_14 from "../../assets/headshots/headshot_14.jpg";
-import headshot_15 from "../../assets/headshots/headshot_15.jpg";
-import headshot_16 from "../../assets/headshots/headshot_16.jpg";
-import headshot_17 from "../../assets/headshots/headshot_17.jpg";
-import headshot_18 from "../../assets/headshots/headshot_18.jpg";
-import headshot_19 from "../../assets/headshots/headshot_19.jpg";
-import headshot_20 from "../../assets/headshots/headshot_20.jpg";
-import headshot_21 from "../../assets/headshots/headshot_21.jpg";
-import headshot_22 from "../../assets/headshots/headshot_22.jpg";
-import headshot_23 from "../../assets/headshots/headshot_23.jpg";
-import headshot_24 from "../../assets/headshots/headshot_24.jpg";
-
 import HeroBtn from '../buttons/HeroBtn';
 
-const volunteersArr = [
-    {
-        name: "Ashley Westgate",
-        headshot: headshot_1,
-        contact: "ashwestgate@email.com"
-    },
-    {
-        name: "Bradley Newborn",
-        headshot: headshot_2,
-        contact: "(480) 555-5555"
-    },
-    {
-        name: "Cameron Thompson",
-        headshot: headshot_3,
-        contact: "cameronthompson@email.com"
-    },
-    {
-        name: "Danielle Johnson",
-        headshot: headshot_4,
-        contact: "(702) 555-5555"
-    },
-    {
-        name: "Ethan Martinez",
-        headshot: headshot_5,
-        contact: "ethanmartinez@email.com"
-    },
-    {
-        name: "Fiona Parker",
-        headshot: headshot_6,
-        contact: "(415) 555-5555"
-    },
-    {
-        name: "Gabriel Smith",
-        headshot: headshot_7,
-        contact: "gabrielsmith@email.com"
-    },
-    {
-        name: "Hannah Wilson",
-        headshot: headshot_8,
-        contact: "(305) 555-5555"
-    },
-    {
-        name: "Isaac Adams",
-        headshot: headshot_9,
-        contact: "isaacadams@email.com"
-    },
-    {
-        name: "Julia Brown",
-        headshot: headshot_10,
-        contact: "(404) 555-5555"
-    },
-    {
-        name: "Kaleb Miller",
-        headshot: headshot_11,
-        contact: "kalebmiller@email.com"
-    },
-    {
-        name: "Lily Davis",
-        headshot: headshot_12,
-        contact: "(212) 555-5555"
-    },
-    {
-        name: "Michaela Garcia",
-        headshot: headshot_13,
-        contact: "michaelagarcia@email.com"
-    },
-    {
-        name: "Nathan Lee",
-        headshot: headshot_14,
-        contact: "(817) 555-5555"
-    },
-    {
-        name: "Olivia Martinez",
-        headshot: headshot_15,
-        contact: "oliviamartinez@email.com"
-    },
-    {
-        name: "Patrick Johnson",
-        headshot: headshot_16,
-        contact: "(603) 555-5555"
-    },
-    {
-        name: "Quincy Taylor",
-        headshot: headshot_17,
-        contact: "quincytaylor@email.com"
-    },
-    {
-        name: "Rachel White",
-        headshot: headshot_18,
-        contact: "(916) 555-5555"
-    },
-    {
-        name: "Samuel Clark",
-        headshot: headshot_19,
-        contact: "samuelclark@email.com"
-    },
-    {
-        name: "Tessa Adams",
-        headshot: headshot_20,
-        contact: "(503) 555-5555"
-    },
-    {
-        name: "Ulysses Martin",
-        headshot: headshot_21,
-        contact: "ulyssesmartin@email.com"
-    },
-    {
-        name: "Victoria Smith",
-        headshot: headshot_22,
-        contact: "(702) 555-5555"
-    },
-    {
-        name: "William Brown",
-        headshot: headshot_23,
-        contact: "williambrown@email.com"
-    },
-    {
-        name: "Xavier Johnson",
-        headshot: headshot_24,
-        contact: "(415) 555-5555"
-    },
-];
 
 
 
 export default function Volunteers() {
 
+    const navigate = useNavigate()
+
     const [volunteers, setVolunteers] = useState([])
     const authHeader = useAuthHeader()
 
     useEffect(() => {
-        console.log(authHeader())
         fetch("/api/volunteers", {
             method: "GET",
             headers: {
@@ -182,38 +35,33 @@ export default function Volunteers() {
             })
     }, [])
     return (
-        <div>
+        <main>
             <HeroBtn route="/" text="back to dashboard" />
-            <HeroBtn route="/add-volunteer" text="add volunteer" />
 
-            <table className="table">
-                <thead>
-                    <tr className="volunteerRow">
-                        <th scope="col"></th>
-                        <th scope="col">name</th>
-                        <th scope="col">preferred contact</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {volunteers.map((volunteer, i) => {
-                        return (
-                            <Link to={`/volunteers/${volunteer.volunteer_id}`}>
-                                <tr className="volunteerRow" key={i}>
-                                    <td>
-                                        {/* <img
+
+            <div className="volunteersGrid">
+                <div className="volunteersGridHeader">
+                    <span>image</span>
+                    <span>name</span>
+                    <span>preferred contact</span>
+                </div>
+                {volunteers.map((volunteer, i) => {
+                    return (
+                        <div className="volunteersGridRow" onClick={() => navigate(`/volunteers/${volunteer.volunteer_id}`)} key={i}>
+                            <span>
+                                {/* <img
                                         className="volunteerImage"
-                                        src={`{volunteer.image_url}`}
+                                        src={`{ volunteer.image_url }`}
                                         alt={volunteer.first_name}
                                     ></img> */}
-                                    </td>
-                                    <td className="volunteerName">{volunteer.first_name} {volunteer.last_name}</td>
-                                    <td className="volunteerContact">{volunteer.contact}</td>
-                                </tr>
-                            </Link>
-                        );
-                    })}
-                </tbody>
-            </table>
-        </div >
+                            </span>
+                            <span className="volunteerName">{volunteer.first_name} {volunteer.last_name}</span>
+                            <span className="volunteerContact">{volunteer.contact}</span>
+                        </div>
+                    );
+                })}
+            </div>
+            <HeroBtn route="/add-volunteer" text="add volunteer" />
+        </main>
     );
 }
