046db5c0742046e6ebc5158ddf34c56653662de2
Chalon Hutson <chalonhutson@gmail.com>
2023-10-12 14:08:35 -0600

    Fixed bug with blank volunteer and no notes.

--- client/src/components/pages/UpdateVolunteer.jsx
+++ client/src/components/pages/UpdateVolunteer.jsx
@@ -17,7 +17,8 @@ export default function UpdateVolunteer() {
             contact: "none"
         },
         phones: [],
-        emails: []
+        emails: [],
+        notes: []
     })
 
     const { volunteerId } = useParams()
