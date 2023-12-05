70057a77f9c8faeff4c98513413e52037c3efc93
Chalon Hutson <chalonhutson@gmail.com>
2023-12-04 17:58:27 -0700

    Progress on donor list component.

--- client/src/components/pages/fundraising/donorList/DonorListEntry.jsx
+++ client/src/components/pages/fundraising/donorList/DonorListEntry.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { FormCheck } from 'react-bootstrap'
 
-export default function DonorListEntry() {
+export default function DonorListEntry({ donor }) {
     return (
         <span className="donorListEntryContainer">
+            <p className="donorListEntryName">{donor.name}</p>
+            <FormCheck defaultChecked={donor.business} className="donorListEntryCheckbox" type="checkbox" />
+            <p className="donorListEntryStatus">{donor.status}</p>
+            <p className="donorListEntryAmount">${donor.expected_amount}</p>
 
         </span>
     )
--- client/src/components/pages/fundraising/donorList/DonorsList.jsx
+++ client/src/components/pages/fundraising/donorList/DonorsList.jsx
@@ -1,10 +1,18 @@
 import { useState, useEffect } from "react"
 import { Button } from "react-bootstrap"
 
+import DonorListEntry from "./DonorListEntry"
+
+import "./DonorsList.css"
+
 export default function DonorsList() {
 
     const [donors, setDonors] = useState([
-
+        { id: 1, name: "John Wick", business: false, expected_amount: 1000, status: "prospect" },
+        { id: 2, name: "Jack Bauer", business: false, expected_amount: 500, status: "donor" },
+        { id: 3, name: "John McClane", business: false, expected_amount: 1000, status: "prospect" },
+        { id: 4, name: "Innotech Inc.", business: true, expected_amount: 5000, status: "prospect" },
+        { id: 5, name: "Globex Corp.", business: true, expected_amount: 10000, status: "super donor" }
     ])
     return (
         <div>
@@ -12,7 +20,9 @@ export default function DonorsList() {
                 <Button className="donorTopSectionButton" variant="primary">Add Donor</Button>
             </span>
             <div className="donorListContainer">
-
+                {donors.map((donor) => {
+                    return <DonorListEntry donor={donor} />
+                })}
             </div>
         </div>
     )
